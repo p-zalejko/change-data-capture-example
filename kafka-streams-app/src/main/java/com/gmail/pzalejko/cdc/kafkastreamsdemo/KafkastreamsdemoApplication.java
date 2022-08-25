59b40ee3651bd07dc7b2f52af750e1e8878a427d
Pawel Zalejko <p.zalejko@gmail.com>
2022-08-25 20:53:28 +0200

    kafka streams init

---
+++ kafka-streams-app/src/main/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/KafkastreamsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.pzalejko.cdc.kafkastreamsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkastreamsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkastreamsdemoApplication.class, args);
+	}
+
+}
---
+++ kafka-streams-app/src/test/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/KafkastreamsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.pzalejko.cdc.kafkastreamsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkastreamsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
