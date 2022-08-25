8858f51c175dcca71ab0057b2aa48b472201c921
Pawel Zalejko <p.zalejko@gmail.com>
2022-08-25 22:28:34 +0200

    Foreign-Key Join Feature

--- kafka-streams-app/src/main/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/processor/WordCountProcessor.java
+++ kafka-streams-app/src/main/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/processor/WordCountProcessor.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.ForeachAction;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueJoiner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import postgres.demo.account.Envelope;
@@ -17,8 +18,7 @@ public class WordCountProcessor {
 
     @Autowired
     void buildPipeline(StreamsBuilder streamsBuilder) {
-        KStream<GenericRecord, Envelope> messageStream = streamsBuilder
-                .stream("postgres.demo.account");
+        KStream<GenericRecord, Envelope> messageStream = streamsBuilder.stream("postgres.demo.account");
 
         messageStream
                 .peek(new ForeachAction<GenericRecord, Envelope>() {
@@ -28,5 +28,37 @@ public class WordCountProcessor {
                     }
                 })
                 .to("output-topic");
+    }
+
+// https://stackoverflow.com/questions/56939323/kafka-stream-dsl-non-key-join-current-workaround-explained
+    // https://www.confluent.io/blog/data-enrichment-with-kafka-streams-foreign-key-joins/
+    void ddd(StreamsBuilder builder){
+        KStream<String, String> account = builder.stream("postgres.demo.account");
+        KStream<String, String> accountOwner = builder.stream("postgres.demo.account_owner");
+
+        ValueJoiner<String, String, String> valueJoiner = (leftValue, rightValue) -> {
+            return leftValue + rightValue;
+        };
+        leftStream.join(rightStream,
+                valueJoiner,
+                JoinWindows.of(Duration.ofSeconds(10)));
+
+
+    }
+    @StreamListener
+    @SendTo(Bindings.RATED_MOVIES)
+    KStream<Long, RatedMovie> rateMoviesFor(@Input(Bindings.AVG_TABLE) KTable<Long, Double> ratings,
+                                            @Input(Bindings.MOVIES) KTable<Long, Movie> movies) {
+
+        ValueJoiner<Movie, Double, RatedMovie> joiner = (movie, rating) ->
+                new RatedMovie(movie.getMovieId(), movie.getReleaseYear(), movie.getTitle(), rating);
+
+        movies
+                .join(ratings, joiner, Materialized
+                        .<Long, RatedMovie, KeyValueStore<Bytes, byte[]>>as(Bindings.RATED_MOVIES_STORE)
+                        .withKeySerde(Serdes.Long())
+                        .withValueSerde(new JsonSerde<>(RatedMovie.class)));
+
+        return movies.join(ratings, joiner).toStream();
     }
 }
