995d336fca1015a5ed848543ca713098d80dfc9d
Pawel Zalejko <p.zalejko@gmail.com>
2022-08-26 22:02:48 +0200

    Foreign-Key Join Feature

--- kafka-streams-app/src/main/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/processor/AccountChangesProcessor.java
+++ kafka-streams-app/src/main/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/processor/AccountChangesProcessor.java
@@ -6,7 +6,6 @@ import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serde;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
