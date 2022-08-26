f16d2acfda1ea9ed78c8f95a8078f8e578e6423e
Pawel Zalejko <p.zalejko@gmail.com>
2022-08-26 22:11:48 +0200

    Foreign-Key Join Feature

--- kafka-streams-app/src/test/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/KafkaStreamsDemoApplicationTests.java
+++ kafka-streams-app/src/test/java/com/gmail/pzalejko/cdc/kafkastreamsdemo/KafkaStreamsDemoApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.gmail.pzalejko.cdc.kafkastreamsdemo;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class KafkaStreamsDemoApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.gmail.pzalejko.cdc.kafkastreamsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
